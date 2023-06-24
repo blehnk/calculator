2eb0f5c87fa30c33bf384649f1e6f6b20c5dc6d9
Ashish Nath <otakuash11apr@gmail.com>
2023-06-24 13:35:30 +0530

    Create keys, and display UI for the calculator

--- script.js
+++ script.js
@@ -3,6 +3,19 @@ let num1;
 let num2;
 let op;
 
+const keys = document.querySelector('#keys');
+
+//creating keys for the calc
+for(let i = 0; i < 10; i++){
+    const div = document.createElement('div');
+    div.textContent = i;
+    div.classList = "key";
+    keys.insertBefore(div, keys.children[0]);
+}
+
+const equalBtn = document.createElement('button');
+
+
 /*HERE ARE ALL THE FUNCTIONS*/ 
 function add(num1, num2){
     return num1 + num2;
@@ -37,3 +50,9 @@ function operate(num1, num2, op){
         divide(num1, num2);
     }
 }
+
+
+/*Here is the DOM Manipulation */
+equalBtn.textContent = 'CLR';
+equalBtn.classList = "key"
+keys.appendChild(equalBtn);
